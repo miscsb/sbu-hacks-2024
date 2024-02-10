82f412770480bb50712fe851459690714edc97be
Samuel Buena <sbuena@cs.stonybrook.edu>
2024-02-10 13:23:41 -0500

    merge conflict

--- backend/application.py
+++ backend/application.py
@@ -1,14 +1,14 @@
-from flask import Flask, request, render_template, make_response, redirect, jsonify
+from flask import Flask, abort, request, render_template, make_response, redirect, jsonify
 import joblib 
 import json
 import sys
 from db import create_user, get_session
+from bson.objectid import ObjectId
 
 application = Flask(__name__)
 
 @application.route('/sessions', methods=['GET', 'POST'])
 def processSession():
-
     if request.method == "POST":
         
         payloadType = request.headers.get('Content-Type')
@@ -20,14 +20,16 @@ def processSession():
     if request.method == "GET":
         return 'This is a GET request test'
         data = request.get_json() 
-    
     return 'TEST RETURN AMOUNT'''
     
-@application.route('/test', methods=['GET'])
-def test_create():
+@application.route('/users', methods=['POST'])
+def prcoess_create_user():
     result = create_user()
     print(result)
-    return jsonify({'user_id': str(result)})
+    if isinstance(result, ObjectId):
+        return jsonify({'user_id': str(result)})
+    else:
+        return abort(401, 'Could not create user.')
 
 if __name__ == '__main__':
     application.run(debug=True) # deployment: remove debug=True

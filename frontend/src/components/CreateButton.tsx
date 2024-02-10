c233915bc35bdc69f1f91ae643b2038e096a9bd5
vincenzo-sorcigli <57498723+vincenzo-sorcigli@users.noreply.github.com>
2024-02-10 13:36:24 -0500

    create summary form

--- frontend/src/components/CreateButton.tsx
+++ frontend/src/components/CreateButton.tsx
@@ -1,15 +1,24 @@
 "use client";
 
 import styles from './CreateButton.module.css';
+import CreateModal from './CreateModal';
+
+import { useState } from 'react';
 
 import { FaPlusCircle } from "react-icons/fa";
 
 const CreateButton = () => {
+
     return (  
-        <button className={styles['create-button']}>
-            <FaPlusCircle />
-        </button>
+        <CreateModal 
+            trigger={
+                <button className={styles['create-button']}>
+                    <FaPlusCircle />
+                </button>
+            }
+        />
     );
+
 }
  
 export default CreateButton;

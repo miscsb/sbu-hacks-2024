e3fb1b4b5080a9ef7452e4dc79bf38a5386a9228
vincenzo-sorcigli <57498723+vincenzo-sorcigli@users.noreply.github.com>
2024-02-09 22:57:46 -0500

    basic layout

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+
+    return ( 
+        <nav className={styles.navbar}>
+            
+        </nav>
+    );
+
+}
+ 
+export default Navbar;
---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import styles from './Sidebar.module.css';
+
+const Sidebar = () => {
+    return ( 
+        <div className={styles.sidebar}>
+            
+        </div>
+    );
+}
+ 
+export default Sidebar;

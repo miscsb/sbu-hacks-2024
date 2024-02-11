541078c62d1205d73222a892a6589536e145d9b3
vincenzo-sorcigli <57498723+vincenzo-sorcigli@users.noreply.github.com>
2024-02-10 22:47:57 -0500

    being created modal

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,8 @@
 export default function HomePage() {
 
-  return <h1>Homepage</h1>;
+  return <div className="homepage w-full h-full flex flex-col items-center justify-center">
+    <h1>Welcome to the Lecture Summarizer.</h1>
+    <h3>Create a new summary to get started.</h3>
+  </div>
 
 }
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import styles from './Sidebar.module.css';
 
 import { useParams } from 'next/navigation';
+import CreateButton from './CreateButton';
 
 const Sidebar = () => {
 
@@ -45,19 +46,89 @@ const Sidebar = () => {
             id: '3',
             title: "Test summary 3",
         },
+        {
+            id: '4',
+            title: "Test summary 4",
+        },
+        {
+            id: '5',
+            title: "Test summary 5",
+        },
+        {
+            id: '6',
+            title: "Test summary 6",
+        },
+        {
+            id: '7',
+            title: "Test summary 7",
+        },
+        {
+            id: '8',
+            title: "Test summary 8",
+        },
+        {
+            id: '9',
+            title: "Test summary 9",
+        },
+        {
+            id: '10',
+            title: "Test summary 10",
+        },
+        {
+            id: '11',
+            title: "Test summary 11",
+        },
+        {
+            id: '12',
+            title: "Test summary 12",
+        },
+        {
+            id: '13',
+            title: "Test summary 13",
+        },
+        {
+            id: '14',
+            title: "Test summary 14",
+        },
+        {
+            id: '15',
+            title: "Test summary 15",
+        },
+        {
+            id: '16',
+            title: "Test summary 16",
+        },
+        {
+            id: '17',
+            title: "Test summary 17",
+        },
+        {
+            id: '18',
+            title: "Test summary 18",
+        },
+        {
+            id: '19',
+            title: "Test summary 19",
+        },
+        {
+            id: '20',
+            title: "Test summary 20",
+        },
     ]
 
     return ( 
         <div className={styles.sidebar}>
-            {summaries.map((summary) => (
-                <Link key={summary.id} href={`/summary/${summary.id}`}>
-                    <div 
-                        className={`${styles.title} ${summary.id === currentId ? styles['active'] : ''}`}
-                    >
-                        {summary.title}
-                    </div>
-                </Link>
-            ))}
+            <div className={styles.summaries}>
+                {summaries.map((summary) => (
+                    <Link key={summary.id} href={`/summary/${summary.id}`}>
+                        <div 
+                            className={`${styles.title} ${summary.id === currentId ? styles['active'] : ''}`}
+                        >
+                            {summary.title}
+                        </div>
+                    </Link>
+                ))} 
+            </div>  
         </div>
     );
 }

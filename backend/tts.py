c82247faa2234d9b14bdff1553b1b0b15a9bf0ee
Samuel Buena <57194851+miscsb@users.noreply.github.com>
2024-02-11 12:28:27 -0500

    Update tts.py

--- backend/tts.py
+++ backend/tts.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 import os
 
-input = "test text: I am a blue kangaroo and I have homosexual tendencies and I love rice pudding. I would like sam to glaze me with peanut butter and push his commit into my git's gyat."
+input = "test text: Hey. Welcome to EchoNotes."
 tts = gTTS(text=input, lang='en', slow=False)
 tts.save("response.mp3")
 os.system("mpg123 response.mp3") # brew install mp123

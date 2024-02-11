f05d6da74e67c05268e462f86379a789d5cc346f
Samuel Buena <sbuena@cs.stonybrook.edu>
2024-02-11 01:51:20 -0500

    add bionic reading depending on query

--- frontend/src/app/summary/[summaryId]/page.tsx
+++ frontend/src/app/summary/[summaryId]/page.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Markdown from "@/components/Markdown";
+import { useSearchParams } from "next/navigation";
 
 type Props = {
     params: {
@@ -7,7 +10,9 @@ type Props = {
     searchParams: URLSearchParams;
 }
 
-const SummaryPage = async ({params: { summaryId }, searchParams}: Props) => {
+const SummaryPage = async ({params: { summaryId }}: Props) => {
+
+    const searchParams = useSearchParams()
 
     type Summary = {
         id: string;
@@ -15,24 +20,51 @@ const SummaryPage = async ({params: { summaryId }, searchParams}: Props) => {
         text_content: string;
     }
 
-    // const res = await fetch(`http://127.0.0.1:5000/summaries/${summaryId}`, {
-    //     method: 'GET',
-    // });
+    const res = await fetch(`http://127.0.0.1:5000/summaries/${summaryId}`, {
+        method: 'GET',
+    });
 
-    // const summary: Summary = await res.json();
-    const summary = {
-        id: '1',
-        title: "Test summary 1",
-        text_content: "# hi\n- test",
-    }
+    const summary: Summary = await res.json();
+    // const summary = {
+    //     id: '1',
+    //     title: "Test summary 1",
+    //     text_content: "# hi\n- test",
+    // }
+
+    var bionic = searchParams.get('bionic') ?? "false"
 
     return (
         <div>
             <h1 className="mb-8 text-4xl underline">{summary.title}</h1>
-            <Markdown markdown={summary.text_content} />
+            <Markdown markdown={bionic == 'true' ? make_bionic_content(summary.text_content) : summary.text_content} />
         </div>
     );
 
+}
+
+const is_word = (word : string) => {
+    return word.match('[a-zA-Z]+') != null;
+}
+
+const make_bionic_word = (word : string) => {
+    var mid = (1 + word.length) / 2;
+    return '**' + word.substring(0, mid) + '**' + word.substring(mid);
+}
+
+const make_bionic_line = (line : string) => {
+    if (line.startsWith('#')) {
+        return line;
+    } else {
+        var items = line.split(' ');
+        items = items.map((item) => is_word(item) ? make_bionic_word(item) : item);
+        return items.join(' ');
+    }
+}
+    
+const make_bionic_content = (text : string) => {
+    var lines = text.split('\n');
+    lines = lines.map(make_bionic_line);
+    return lines.join('\n');
 }
  
 export default SummaryPage;
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -6,7 +6,7 @@ import styles from './Sidebar.module.css';
 import { useParams } from 'next/navigation';
 import CreateButton from './CreateButton';
 
-const Sidebar = () => {
+const Sidebar = async () => {
 
     const params = useParams();
 
@@ -27,27 +27,27 @@ const Sidebar = () => {
 
     // get all summaries
 
-    // const res = await fetch('http://127.0.0.1:5000/summaries', {
-    //     method: 'GET',
-    // });
+    const res = await fetch('http://127.0.0.1:5000/summaries', {
+        method: 'GET',
+    });
 
-    // const summaries: Summary[] = await res.json();
+    const summaries: Summary[] = await res.json();
 
    
-    const summaries = [
-        {
-            id: '1',
-            title: "Test summary 1",
-        },
-        {
-            id: '2',
-            title: "Test summary 2",
-        },
-        {
-            id: '3',
-            title: "Test summary 3",
-        },
-    ]
+    // const summaries = [
+    //     {
+    //         id: '1',
+    //         title: "Test summary 1",
+    //     },
+    //     {
+    //         id: '2',
+    //         title: "Test summary 2",
+    //     },
+    //     {
+    //         id: '3',
+    //         title: "Test summary 3",
+    //     },
+    // ]
 
     return ( 
         <div className={styles.sidebar}>
